f626a84ce2df869a4bfd2d775acf892ed3327dc7
Emine İnan <emineinanx@gmail.com>
2021-07-15 10:52:23 +0300

    Design UpdateFragment layout

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -22,6 +22,10 @@ class ListFragment : Fragment() {
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+
+        binding.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
         return binding.root
     }
 }
