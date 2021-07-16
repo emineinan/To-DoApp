72fea20662464edff1223f074b1de57421f05c0d
Emine İnan <emineinanx@gmail.com>
2021-07-16 11:48:14 +0300

    Create Dao class

---
+++ app/src/main/java/com/example/todoapp/data/ToDoDao.kt
@@ -0,0 +1,15 @@
+package com.example.todoapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.todoapp.data.models.ToDoData
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(toDoData: ToDoData)
+}
