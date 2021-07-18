cfe1ea76d41e06371a36fdb69d9f02907865fcc0
Emine İnan <emineinanx@gmail.com>
2021-07-18 11:22:06 +0300

    Display data in Recyclerview

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -3,13 +3,20 @@ package com.example.todoapp.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoapp.R
+import com.example.todoapp.data.viewmodel.ToDoViewModel
 import com.example.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
+
+    private val adapter: ListAdapter by lazy { ListAdapter() }
+    private val toDoViewModel: ToDoViewModel by viewModels<ToDoViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +24,16 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        //Set Adapter
+        val recyclerView=binding.recyclerView
+        recyclerView.adapter=adapter
+        recyclerView.layoutManager=LinearLayoutManager(requireActivity())
+
+        toDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data->
+            adapter.setData(data)
+        })
+
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
@@ -31,6 +48,6 @@ class ListFragment : Fragment() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.list_fragment_menu,menu)
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
