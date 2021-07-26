0b9c9b6436193e9f478ef9ea11392c807e10f354
Emine İnan <emineinanx@gmail.com>
2021-07-26 10:45:44 +0300

    Fix app crash due to undo function

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -118,21 +118,20 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
                 ).show()
 
                 //Restore Deleted Item
-                restoreDeletedData(viewHolder.itemView, deletedItem, viewHolder.adapterPosition)
+                restoreDeletedData(viewHolder.itemView, deletedItem)
             }
         }
         val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
         itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
-    private fun restoreDeletedData(view: View, deletedItem: ToDoData, position: Int) {
+    private fun restoreDeletedData(view: View, deletedItem: ToDoData) {
         val snackbar = Snackbar.make(
             view, "Deleted ${deletedItem.title}",
             Snackbar.LENGTH_LONG
         )
         snackbar.setAction("Undo") {
             toDoViewModel.insertData(deletedItem)
-            adapter.notifyItemChanged(position)
         }
         snackbar.show()
     }
