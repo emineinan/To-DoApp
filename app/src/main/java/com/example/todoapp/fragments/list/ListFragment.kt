01ea3424e31a07dd2bf223ed2cee8d3e3a23c244
Emine İnan <emineinanx@gmail.com>
2021-07-26 11:00:43 +0300

    Hide keyboard

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -16,6 +16,7 @@ import com.example.todoapp.data.viewmodel.ToDoViewModel
 import com.example.todoapp.databinding.FragmentListBinding
 import com.example.todoapp.fragments.SharedViewModel
 import com.example.todoapp.fragments.list.adapter.ListAdapter
+import com.example.todoapp.utils.hideKeyboard
 import com.google.android.material.snackbar.Snackbar
 
 class ListFragment : Fragment(), SearchView.OnQueryTextListener {
@@ -50,6 +51,9 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
 
         //Set menu
         setHasOptionsMenu(true)
+
+        //Hide keyboard
+        hideKeyboard(requireActivity())
 
         return binding.root
     }
